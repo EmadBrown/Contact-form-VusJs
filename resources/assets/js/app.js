a3cd198fa2d59482cff86145455452e6ea219ae2
EmadMRI <emad.it@protonmail.com>
2018-01-16 16:20:17 +0100

    Setup Contact component / customiz welcome view blade

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,4 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -16,6 +10,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('contact', require('./components/Contact.vue'));
 
 const app = new Vue({
     el: '#app'
